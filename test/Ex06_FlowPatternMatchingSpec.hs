519018daeff8ed4863aaad4120b1abb26415dcd2
Luis Naia <luisnaia@protonmail.com>
2019-01-29 22:36:15 +0000

    Tue Jan 29 22:36:15 GMT 2019

--- test/Ex06_FlowPatternMatchingSpec.hs
+++ test/Ex06_FlowPatternMatchingSpec.hs
@@ -7,47 +7,76 @@ import Test.Hspec
 main :: IO ()
 main = hspec spec
 
+-- factorial
+factorial :: Int -> Int
+factorial 0 = 1
+factorial 5 = 120
+factorial x = x * (factorial (x - 1))
+
+charName :: Char -> [Char]
+charName 'a' = "Albert"
+charName _ = "What do you mean?"
+
+addVectors :: (Int, Int) -> (Int, Int) -> (Int, Int)
+-- addVectors x y = (fst x + fst y, snd x + snd y)
+addVectors (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
+
+first :: (Int, Int, Int) -> Int
+first (x, _, _) = x
+second (_, x, _) = x
+third (_, _, x) = x
+
+
+head' :: [a] -> a
+head' (x:_) = x
+
+tell :: [Int] -> [Char]
+tell [] = "This list is empty"
+tell (x:_) = "This list has one element: 1"
+
+length' :: [Int] -> Int
+length' [] = 0
+length' (x:xs) =  1 + length' xs
+
+reduce' :: [Int] -> Int
+reduce' [] = 0
+reduce' (x:xs) =  x + reduce' xs
+
 spec :: Spec
 spec = do
   describe "Pattern matching" $ do
     it "can be used in factorial calc" $ do
-      pending
-      -- factorial 5 `shouldBe` 120
+     factorial 5 `shouldBe` 120
     it "can fail when no default case" $ do
-      pending
-      -- charName 'a' `shouldBe` "Albert"
-      -- charName'd' `shouldThrow` PatternMatchFail
+      charName 'a' `shouldBe` "Albert"
+      charName '?' `shouldBe` "What do you mean?"
     it "can be used on tuples" $ do
-      pending
-      -- addVectors (1,2)(3,4) `shouldBe` (4,6)
+      addVectors (1,2)(3,4) `shouldBe` (4,6)
     it "can be used on triples" $ do
-      pending
-      -- first (1,2,3) `shouldBe` 1
-      -- second (1,2,3) `shouldBe` 2
-      -- third (1,2,3) `shouldBe` 3
+      first (1,2,3) `shouldBe` 1
+      second (1,2,3) `shouldBe` 2
+      third (1,2,3) `shouldBe` 3
     it "can pattern list comprehensions" $ do
-      pending
-      -- let xs = [(1,3),(4,3),(2,4),(5,3),(5,6),(3,1)]
-      -- [a+b | (a,b) <- xs] `shouldBe` [4,7,6,8,11,4]
+      let xs = [(1,3),(4,3),(2,4),(5,3),(5,6),(3,1)]
+      [a+b | (a,b) <- xs] `shouldBe` [4,7,6,8,11,4]
     it "can be used for the head function" $ do
-      pending
-      -- head' [2,3,4] `shouldBe` 2
-      -- head' "Hello" `shouldBe` 'H'
+      head' [2,3,4] `shouldBe` 2
+      head' "Hello" `shouldBe` 'H'
     it "can safely process a list" $ do
-      pending
-      -- tell [] `shouldBe` "This list is empty"
-      -- tell [1] `shouldBe` "This list has one element: 1"
+      tell [] `shouldBe` "This list is empty"
+      tell [1] `shouldBe` "This list has one element: 1"
       -- tell [1,2] `shouldBe` "This list has two elements: 1 and 2"
       -- tell [1,2,3] `shouldBe` "This list is too long"
     it "can count elements in list with recursion" $ do
-      pending
-      -- length' [] `shouldBe` 0
-      -- length' [1,2,3] `shouldBe` 3
+      length' [] `shouldBe` 0
+      length' [1,2,3] `shouldBe` 3
     it "can reduce add a list" $ do
-      pending
-      -- sum' [] `shouldBe` 0
-      -- sum' [1,2,3] `shouldBe` 6
+      reduce' [] `shouldBe` 0
+      reduce' [1,2,3] `shouldBe` 6
     it "can hold the original item with pattern" $ do
-      pending
-      -- firstLetter "" `shouldBe` "Empty string, whoops!"
-      -- firstLetter "Dracula" `shouldBe` "The first letter of Dracula is D"
+      firstLetter "" `shouldBe` "Empty string, whoops!"
+      firstLetter "Dracula" `shouldBe` "The first letter of Dracula is D"
+
+
+      firstLetter :: [Char] -> [Char]
+      firstLetter [] = "Empty string, whoops!"
